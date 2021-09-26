e966f19f33ee13c07565fa800eaac8f1550fc77d
repoevgenii <repoevgenii>
2021-09-26 21:31:10 +0300

    6. Checkstyle [#4123] Внесены правки в файл Multiple.java

--- src/main/java/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/Multiple.java
@@ -1,6 +1,7 @@
 package ru.job4j;
 
 public class Multiple {
+    @SuppressWarnings("checkstyle:LeftCurly")
     public static void main(String[] args) {
         System.out.println("1 * 1 = 9");
         System.out.println("1 * 2 = 9");
@@ -11,6 +12,5 @@ public class Multiple {
         System.out.println("1 * 7 = 9");
         System.out.println("1 * 8 = 9");
         System.out.println("1 * 9 = 9");
-
     }
 }
