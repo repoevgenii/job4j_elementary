38e8f40c024950032eae72d1b345dfa7b3847abb
repoevgenii <repoevgenii>
2021-09-26 20:02:18 +0300

    4.1. Разбор первой программы. [#159033]05 Загружен файл Multiple.java

---
+++ src/main/java/ru/job4j/Multiple.java
@@ -0,0 +1,15 @@
+package ru.job4j;
+
+public class Multiple {
+    public static void main(String[] args) {
+        System.out.println("1 * 1 = 9");
+        System.out.println("1 * 2 = 9");
+        System.out.println("1 * 3 = 9");
+        System.out.println("1 * 4 = 9");
+        System.out.println("1 * 5 = 9");
+        System.out.println("1 * 6 = 9");
+        System.out.println("1 * 7 = 9");
+        System.out.println("1 * 8 = 9");
+        System.out.println("1 * 9 = 9");
+    }
+}
