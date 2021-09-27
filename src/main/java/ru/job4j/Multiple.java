181658f5ab4e8c330ba7e658868b30c596aaef5c
repoevgenii <repoevgenii>
2021-09-27 22:58:04 +0300

    4.1. Разбор первой программы. [#159033 #208768] Внесены правки в файл Multiple.java

--- src/main/java/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/Multiple.java
@@ -1,16 +1,15 @@
 package ru.job4j;
 
 public class Multiple {
-    @SuppressWarnings("checkstyle:LeftCurly")
     public static void main(String[] args) {
-        System.out.println("1 * 1 = 9");
-        System.out.println("1 * 2 = 9");
-        System.out.println("1 * 3 = 9");
-        System.out.println("1 * 4 = 9");
-        System.out.println("1 * 5 = 9");
-        System.out.println("1 * 6 = 9");
-        System.out.println("1 * 7 = 9");
-        System.out.println("1 * 8 = 9");
+        System.out.println("1 * 1 = 1");
+        System.out.println("1 * 2 = 2");
+        System.out.println("1 * 3 = 3");
+        System.out.println("1 * 4 = 4");
+        System.out.println("1 * 5 = 5");
+        System.out.println("1 * 6 = 6");
+        System.out.println("1 * 7 = 7");
+        System.out.println("1 * 8 = 8");
         System.out.println("1 * 9 = 9");
     }
 }
