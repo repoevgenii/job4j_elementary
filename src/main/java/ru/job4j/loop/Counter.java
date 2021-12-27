8f02966537eae00766e9f5bccd113138872694b2
repoevgenii <repoevgenii>
2021-12-28 01:30:58 +0300

    5.1. Подсчет суммы чётных чисел в диапазоне [#192] Реализован метод sumByEven. Добавлен тест-метод.

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -1,6 +1,7 @@
 package ru.job4j.loop;
 
 public class Counter {
+
     public static int sum(int start, int finish) {
         int sum = 0;
         for (int i = start; i <= finish; i++) {
@@ -9,12 +10,13 @@ public class Counter {
         return sum;
     }
 
-    public static void main(String[] args) {
-        System.out.println(sum(0, 10));
-        System.out.println(sum(3, 8));
-        System.out.println(sum(1, 1));
-        System.out.println(sum(16, 21));
-        System.out.println(sum(-4, 10));
-        System.out.println(sum(0, 5));
+    public static int sumByEven(int start, int finish) {
+        int sum = 0;
+        for (int i = start; i <= finish; i++) {
+            if (i % 2 == 0) {
+                sum = sum + i;
+            }
+        }
+        return sum;
     }
 }
