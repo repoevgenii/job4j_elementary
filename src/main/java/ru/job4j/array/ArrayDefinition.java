15c01937029b0680440a4186fb29f910f64f80a4
repoevgenii <repoevgenii>
2022-05-23 23:36:11 +0300

    6.0.2. Заполнение массива. [#226300] В классе ArrayDefinition создан и заполнен массив String. Содержимое массива выведено в консоль.

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,8 +5,17 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Иван";
+        names[1] = "Мария";
+        names[2] = "Ольга";
+        names[3] = "Дмитрий";
         System.out.println("Размер массива ages равен: " + ages.length);
         System.out.println("Размер массива surnames равен: " + surnames.length);
         System.out.println("Размер массива prices равен: " + prices.length);
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
